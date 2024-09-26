0972512cae3fd3b2a6e2b5232c62fcb86808eea8
Eemeli Koikkalainen <eemeli.koikkalainen@outlook.com>
2024-09-26 13:45:01 +0300

    add other pages too

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,16 @@ import { defineConfig } from "vite";
 import tailwindcss from "tailwindcss";
 
 export default defineConfig({
+	build: {
+		rollupOptions: {
+			input: {
+				main: "./index.html",
+				cv: "./cv.html",
+				hobbies: "./hobbies.html",
+				contact: "./contact.html",
+			},
+		},
+	},
 	base: "",
 	css: {
 		postcss: {
